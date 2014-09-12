8b9ec72dc39212f90ec6464bc6a56a79d0889245
bjhuazhang <bjhuazhang@126.com>
2014-09-12 17:50:32 +0800

    test
    test

---
+++ com/moji/util/zeromq/ZeroMqResBean.java
@@ -0,0 +1,23 @@
+package com.moji.util.zeromq;
+
+import com.google.gson.annotations.Expose;
+
+public class ZeroMqResBean {
+	@Expose
+	private Integer code;
+	@Expose
+	private String msg;
+	
+	public Integer getCode() {
+		return code;
+	}
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}	
+}
